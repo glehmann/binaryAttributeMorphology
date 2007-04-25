afd3b6e15eef90d506bfaf6b2f08c44262e48e3e
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-04-25 12:05:51 +0200

    add a binary reconstruction by erosion filter
    darcs-hash:20070425100551-2fc9d-e48011d8996afd79e78b27cb90550bd5f68c7c75.gz

---
+++ itkBinaryReconstructionByErosionImageFilter.h
@@ -0,0 +1,179 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkBinaryReconstructionByErosionImageFilter.h,v $
+  Language:  C++
+  Date:      $Date: 2006/03/28 19:59:05 $
+  Version:   $Revision: 1.6 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkBinaryReconstructionByErosionImageFilter_h
+#define __itkBinaryReconstructionByErosionImageFilter_h
+
+#include "itkInPlaceImageFilter.h"
+#include "itkLabelObject.h"
+#include "itkLabelCollectionImage.h"
+#include "itkBinaryNotImageFilter.h"
+#include "itkBinaryImageToLabelCollectionImageFilter.h"
+#include "itkReconstructionLabelCollectionImageFilter.h"
+#include "itkLabelCollectionImageToMaskImageFilter.h"
+
+
+namespace itk {
+
+/** \class BinaryReconstructionByErosionImageFilter
+ * \brief Identify local maxima whose height above the baseline is greater than h.
+ *
+ * \ingroup ImageEnhancement  MathematicalMorphologyImageFilters
+ */
+template<class TInputImage>
+class ITK_EXPORT BinaryReconstructionByErosionImageFilter : 
+    public InPlaceImageFilter<TInputImage, TInputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef BinaryReconstructionByErosionImageFilter Self;
+  typedef InPlaceImageFilter<TInputImage, TInputImage>
+  Superclass;
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Some convenient typedefs. */
+  typedef TInputImage InputImageType;
+  typedef TInputImage OutputImageType;
+  typedef typename InputImageType::Pointer         InputImagePointer;
+  typedef typename InputImageType::ConstPointer    InputImageConstPointer;
+  typedef typename InputImageType::RegionType      InputImageRegionType;
+  typedef typename InputImageType::PixelType       InputImagePixelType;
+  typedef typename OutputImageType::Pointer        OutputImagePointer;
+  typedef typename OutputImageType::ConstPointer   OutputImageConstPointer;
+  typedef typename OutputImageType::RegionType     OutputImageRegionType;
+  typedef typename OutputImageType::PixelType      OutputImagePixelType;
+  
+  /** ImageDimension constants */
+  itkStaticConstMacro(InputImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+  itkStaticConstMacro(OutputImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+
+  typedef BinaryNotImageFilter< InputImageType > NotType;
+  typedef LabelObject<unsigned long, ImageDimension> LabelObjectType;
+  typedef typename itk::LabelCollectionImage< LabelObjectType > LabelCollectionImageType;
+  typedef typename itk::BinaryImageToLabelCollectionImageFilter< InputImageType, LabelCollectionImageType > LabelizerType;
+  typedef typename itk::ReconstructionLabelCollectionImageFilter< LabelCollectionImageType, InputImageType > ReconstructionType;
+  typedef typename itk::LabelCollectionImageToMaskImageFilter< LabelCollectionImageType, OutputImageType > BinarizerType;
+
+  /** Standard New method. */
+  itkNewMacro(Self);  
+
+  /** Runtime information support. */
+  itkTypeMacro(BinaryReconstructionByErosionImageFilter, 
+               InPlaceImageFilter);
+
+  /**
+   * Set/Get whether the connected components are defined strictly by
+   * face connectivity or by face+edge+vertex connectivity.  Default is
+   * FullyConnectedOff.  For objects that are 1 pixel wide, use
+   * FullyConnectedOn.
+   */
+  itkSetMacro(FullyConnected, bool);
+  itkGetConstReferenceMacro(FullyConnected, bool);
+  itkBooleanMacro(FullyConnected);
+
+#ifdef ITK_USE_CONCEPT_CHECKING
+  /** Begin concept checking */
+  itkConceptMacro(InputEqualityComparableCheck,
+    (Concept::EqualityComparable<InputImagePixelType>));
+  itkConceptMacro(IntConvertibleToInputCheck,
+    (Concept::Convertible<int, InputImagePixelType>));
+  itkConceptMacro(InputOStreamWritableCheck,
+    (Concept::OStreamWritable<InputImagePixelType>));
+  /** End concept checking */
+#endif
+
+  /**
+   * Set/Get the value used as "background" in the output image.
+   * Defaults to NumericTraits<PixelType>::NonpositiveMin().
+   */
+  itkSetMacro(BackgroundValue, OutputImagePixelType);
+  itkGetConstMacro(BackgroundValue, OutputImagePixelType);
+
+  /**
+   * Set/Get the value used as "foreground" in the output image.
+   * Defaults to NumericTraits<PixelType>::max().
+   */
+  itkSetMacro(ForegroundValue, OutputImagePixelType);
+  itkGetConstMacro(ForegroundValue, OutputImagePixelType);
+
+   /** Set the marker image */
+  void SetMarkerImage(InputImageType *input)
+     {
+     // Process object is not const-correct so the const casting is required.
+     this->SetNthInput( 0, const_cast<InputImageType *>(input) );
+     }
+
+  /** Get the marker image */
+  InputImageType * GetMarkerImage()
+    {
+    return static_cast<InputImageType*>(const_cast<DataObject *>(this->ProcessObject::GetInput(0)));
+    }
+
+   /** Set the marker image */
+  void SetMaskImage(InputImageType *input)
+     {
+     // Process object is not const-correct so the const casting is required.
+     this->SetNthInput( 1, const_cast<InputImageType *>(input) );
+     }
+
+  /** Get the marker image */
+  InputImageType * GetMaskImage()
+    {
+    return static_cast<InputImageType*>(const_cast<DataObject *>(this->ProcessObject::GetInput(1)));
+    }
+
+
+protected:
+  BinaryReconstructionByErosionImageFilter();
+  ~BinaryReconstructionByErosionImageFilter() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** BinaryReconstructionByErosionImageFilter needs the entire input be
+   * available. Thus, it needs to provide an implementation of
+   * GenerateInputRequestedRegion(). */
+  void GenerateInputRequestedRegion() ;
+
+  /** BinaryReconstructionByErosionImageFilter will produce the entire output. */
+  void EnlargeOutputRequestedRegion(DataObject *itkNotUsed(output));
+  
+  /** Single-threaded version of GenerateData.  This filter delegates
+   * to GrayscaleGeodesicErodeImageFilter. */
+  void GenerateData();
+  
+
+private:
+  BinaryReconstructionByErosionImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  bool                m_FullyConnected;
+  OutputImagePixelType m_BackgroundValue;
+  OutputImagePixelType m_ForegroundValue;
+} ; // end of class
+
+} // end namespace itk
+  
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkBinaryReconstructionByErosionImageFilter.txx"
+#endif
+
+#endif
+
+
---
+++ itkBinaryReconstructionByErosionImageFilter.txx
@@ -0,0 +1,130 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkBinaryReconstructionByErosionImageFilter.txx,v $
+  Language:  C++
+  Date:      $Date: 2006/08/01 19:16:18 $
+  Version:   $Revision: 1.7 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkBinaryReconstructionByErosionImageFilter_txx
+#define __itkBinaryReconstructionByErosionImageFilter_txx
+
+#include "itkBinaryReconstructionByErosionImageFilter.h"
+#include "itkProgressAccumulator.h"
+
+
+namespace itk {
+
+template<class TInputImage>
+BinaryReconstructionByErosionImageFilter<TInputImage>
+::BinaryReconstructionByErosionImageFilter()
+{
+  m_BackgroundValue = NumericTraits<OutputImagePixelType>::NonpositiveMin();
+  m_ForegroundValue = NumericTraits<OutputImagePixelType>::max();
+  m_FullyConnected = false;
+  this->SetNumberOfRequiredInputs(2);
+}
+
+template<class TInputImage>
+void 
+BinaryReconstructionByErosionImageFilter<TInputImage>
+::GenerateInputRequestedRegion()
+{
+  // call the superclass' implementation of this method
+  Superclass::GenerateInputRequestedRegion();
+  
+  // We need all the input.
+  InputImagePointer input = const_cast<InputImageType *>(this->GetInput());
+  if( input )
+    {
+    input->SetRequestedRegion( input->GetLargestPossibleRegion() );
+    }
+}
+
+
+template<class TInputImage>
+void 
+BinaryReconstructionByErosionImageFilter<TInputImage>
+::EnlargeOutputRequestedRegion(DataObject *)
+{
+  this->GetOutput()
+    ->SetRequestedRegion( this->GetOutput()->GetLargestPossibleRegion() );
+}
+
+
+template<class TInputImage>
+void
+BinaryReconstructionByErosionImageFilter<TInputImage>
+::GenerateData()
+{
+  // Create a process accumulator for tracking the progress of this minipipeline
+  ProgressAccumulator::Pointer progress = ProgressAccumulator::New();
+  progress->SetMiniPipelineFilter(this);
+
+  // Allocate the output
+  this->AllocateOutputs();
+  
+  typename NotType::Pointer notMask = NotType::New();
+  notMask->SetInput( this->GetMaskImage() );
+  notMask->SetForegroundValue( m_ForegroundValue );
+  notMask->SetBackgroundValue( m_BackgroundValue );
+  progress->RegisterInternalFilter(notMask, .1f);
+  notMask->Update();
+  
+  typename NotType::Pointer notMarker = NotType::New();
+  notMarker->SetInput( this->GetMarkerImage() );
+  notMarker->SetForegroundValue( m_ForegroundValue );
+  notMarker->SetBackgroundValue( m_BackgroundValue );
+  progress->RegisterInternalFilter(notMarker, .1f);
+  notMarker->Update();
+  
+  typename LabelizerType::Pointer labelizer = LabelizerType::New();
+  labelizer->SetInput( notMask->GetOutput() );
+  labelizer->SetForegroundValue( m_ForegroundValue );
+  labelizer->SetBackgroundValue( m_BackgroundValue );
+  labelizer->SetFullyConnected( m_FullyConnected );
+  progress->RegisterInternalFilter(labelizer, .3f);
+  
+  typename ReconstructionType::Pointer reconstruction = ReconstructionType::New();
+  reconstruction->SetInput( labelizer->GetOutput() );
+  reconstruction->SetMarkerImage( notMarker->GetOutput() );
+  reconstruction->SetForegroundValue( m_ForegroundValue );
+  progress->RegisterInternalFilter(reconstruction, .3f);
+  
+  // invert the image during the binarization
+  typename BinarizerType::Pointer binarizer = BinarizerType::New();
+  binarizer->SetInput( reconstruction->GetOutput() );
+  binarizer->SetLabel( m_BackgroundValue );
+  binarizer->SetNegated( true );
+  binarizer->SetBackgroundValue( m_ForegroundValue );
+  binarizer->SetFeatureImage( this->GetMaskImage() );
+  progress->RegisterInternalFilter(binarizer, .2f);
+
+  binarizer->GraftOutput( this->GetOutput() );
+  binarizer->Update();
+  this->GraftOutput( binarizer->GetOutput() );
+}
+
+
+template<class TInputImage>
+void
+BinaryReconstructionByErosionImageFilter<TInputImage>
+::PrintSelf(std::ostream &os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+
+  os << indent << "FullyConnected: "  << m_FullyConnected << std::endl;
+  os << indent << "BackgroundValue: "  << static_cast<typename NumericTraits<OutputImagePixelType>::PrintType>(m_BackgroundValue) << std::endl;
+  os << indent << "ForegroundValue: "  << static_cast<typename NumericTraits<OutputImagePixelType>::PrintType>(m_ForegroundValue) << std::endl;
+}
+  
+}// end namespace itk
+#endif
