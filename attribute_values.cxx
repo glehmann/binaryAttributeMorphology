926bed91f416d36609a1c71cd2c5c17b44fdb36e
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-08-16 12:09:17 +0200

    add a example about how to read the attributes of an object
    darcs-hash:20070816100917-2fc9d-bf1959bbd990c73a6cbe6ca9e61a9bd566ad58c2.gz

---
+++ attribute_values.cxx
@@ -0,0 +1,50 @@
+#include "itkImageFileReader.h"
+#include "itkShapeLabelObject.h"
+#include "itkLabelCollectionImage.h"
+#include "itkBinaryImageToLabelCollectionImageFilter.h"
+#include "itkShapeLabelCollectionImageFilter.h"
+
+int main(int, char * argv[])
+{
+  const int dim = 2;
+  typedef unsigned char PixelType;
+  typedef itk::Image< PixelType, dim >    ImageType;
+  
+  // read the input image
+  typedef itk::ImageFileReader< ImageType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+  
+  // define the object type. Here the ShapeLabelObject type
+  // is chosen in order to read some attribute related to the shape
+  // of the objects (by opposition to the content of the object, with
+  // the StatisticsLabelObejct).
+  typedef unsigned long LabelType;
+  typedef itk::ShapeLabelObject< LabelType, dim > LabelObjectType;
+  typedef itk::LabelCollectionImage< LabelObjectType > LabelCollectionType;
+
+  // convert the image in a collection of objects
+  typedef itk::BinaryImageToLabelCollectionImageFilter< ImageType, LabelCollectionType > ConverterType;
+  ConverterType::Pointer converter = ConverterType::New();
+  converter->SetInput( reader->GetOutput() );
+  converter->SetForegroundValue( 200 );
+
+  // and valuate the attributes with the dedicated filter: ShapeLabelCollectionImageFilter
+  typedef itk::ShapeLabelCollectionImageFilter< LabelCollectionType > ShapeFilterType;
+  ShapeFilterType::Pointer shape = ShapeFilterType::New();
+  shape->SetInput( converter->GetOutput() );
+
+  // update the shape filter, so its output will be up to date
+  shape->Update();
+
+  // then we can read the attribute values we're interested in
+  LabelCollectionType::Pointer collection = shape->GetOutput();
+  for( int label=1; label<collection->GetNumberOfObjects(); label++ )
+    {
+    LabelObjectType::Pointer labelObject = collection->GetLabelObject( label );
+    std::cout << label << "\t" << labelObject->GetPhysicalSize() << "\t" << labelObject->GetCentroid() << std::endl;
+    }
+  
+  return 0;
+}
+
