6364ec5308bed8a8cfc603d04e557af51b895477
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-09-04 14:52:13 +0200

    fix wrong run time info
    darcs-hash:20070904125213-2fc9d-5a85d9b52e5e6a2f3edd231123d6867e483c7482.gz

--- itkLabelObject.h
+++ itkLabelObject.h
@@ -179,7 +179,7 @@ public:
   itkNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(LabelObject, Object);
+  itkTypeMacro(LabelObject, LightObject);
 
   itkStaticConstMacro(ImageDimension, unsigned int, VImageDimension);
 
